a6ac55b4f476643ce5e9b70738aef80aee189afe
Kevin Chao <kevinchao562@yahoo.com>
2016-04-19 00:28:34 -0700

    Fixed consecutive .. in email address validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -39,7 +39,7 @@ class UserTest < ActiveSupport::TestCase
  	end
 
  	test "email validation should reject invalid addresses" do
- 		invalid_addresses = %w[user_at_foo.org user@example,com user.name@example. foo@bar_baz.com foo@bar+baz.com]
+ 		invalid_addresses = %w[user_at_foo.org  user@example..com user@example,com user.name@example. foo@bar_baz.com foo@bar+baz.com]
  		invalid_addresses.each do | invalid_address |
  			@user.email = invalid_address
  			assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
